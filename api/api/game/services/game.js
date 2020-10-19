f01209dbcd62cceea1ca69d946c6da7df0893d40
leonardo silva <leonardohsilva.dev@gmail.com>
2020-10-19 12:54:42 -0300

    data scrapper with JSDOM

--- api/api/game/services/game.js
+++ api/api/game/services/game.js
@@ -7,6 +7,27 @@
 
 const axios = require('axios')
 
+async function getGameInfo(slug) {
+  const { JSDOM } = require('jsdom')
+
+  const body = await axios.get(`https://www.gog.com/game/${slug}`)
+
+  const dom = new JSDOM(body.data)
+
+  const ratingElement = dom.window.document.querySelector('.age-restrictions__icon use')
+
+  const description = dom.window.document.querySelector('.description')
+
+  return {
+    rating: ratingElement ?
+      ratingElement.getAttribute('xlink:href').replace(/_/g, '').replace(/[^\w-]+/g, '') :
+      'FREE',
+    short_description: description.textContent.trim().slice(0, 160),
+    description: description.innerHTML
+  }
+
+}
+
 module.exports = {
   populate: async (params) => {
     const url = 'https://www.gog.com/games/ajax/filtered?mediaType=game&page=1&sort=popularity';
@@ -14,6 +35,8 @@ module.exports = {
     const { data: { products } } = await axios.get(url)
     // .catch(err => console.log('Erro aaa: ', err))
 
-    console.log(products)
+    console.log(products[1].slug)
+
+    console.log(await getGameInfo(products[1].slug))
   }
 };
