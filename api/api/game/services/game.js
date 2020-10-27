7aceab03910e69b2a3d05270472c5e64bbf57d54
leonardo silva <leonardohsilva.dev@gmail.com>
2020-10-27 12:14:52 -0300

    creating games structure and adjusting rating to br

--- api/api/game/services/game.js
+++ api/api/game/services/game.js
@@ -22,7 +22,7 @@ async function getGameInfo(slug) {
   return {
     rating: ratingElement ?
       ratingElement.getAttribute('xlink:href').replace(/_/g, '').replace(/[^\w-]+/g, '') :
-      'FREE',
+      'LIVRE',
     short_description: description.textContent.trim().slice(0, 160),
     description: description.innerHTML
   }
@@ -70,13 +70,45 @@ async function createManyToManyData(products) {
   })
 }
 
+async function createGames(products) {
+  await Promise.all(
+    products.map(async product => {
+      const item = await getByName(product.title, "game");
+
+      if (!item) {
+        console.info(`Creating ${product.title}...`);
+
+        const game = await strapi.services.game.create({
+          name: product.title,
+          slug: product.slug.replace(/_/g, "-"),
+          price: product.price.amount,
+          release_date: new Date(Number(product.globalReleaseDate) * 1000).toISOString(),
+          categories: await Promise.all(product.genres.map(name => getByName(name, "category"))),
+          platforms: await Promise.all(
+            product.supportedOperatingSystems.map(name => getByName(name, "platform"))
+          ),
+          developers: [await getByName(product.developer, "developer")],
+          publisher: await getByName(product.publisher, "publisher"),
+          ...(await getGameInfo(product.slug))
+        });
+
+        return game;
+      }
+    })
+  )
+}
+
 module.exports = {
   populate: async (params) => {
     const url = 'https://www.gog.com/games/ajax/filtered?mediaType=game&page=1&sort=popularity';
 
     const { data: { products } } = await axios.get(url)
 
+    console.log(products[5]);
+
     await createManyToManyData([products[4], products[5]])
+
+    await createGames([products[4], products[5]])
 
     // await create(products[2].publisher, "publisher")
     // await create(products[2].publisher, "developer")
