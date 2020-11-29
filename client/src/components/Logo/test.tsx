e26965db9c97554f9b0dc10967c0fb3d68e5319d
leonardo silva <leonardohsilva.dev@gmail.com>
2020-11-29 17:00:26 -0300

    creating another prop with TDD, working with responsiveness

--- client/src/components/Logo/test.tsx
+++ client/src/components/Logo/test.tsx
@@ -1,5 +1,6 @@
 import { screen } from '@testing-library/react'
 import { renderWithTheme } from '../../utils/tests/helpers'
+import 'jest-styled-components'
 
 import Logo from '.'
 
@@ -12,5 +13,21 @@ describe('<Logo />', () => {
   it('should render a black label when color is passed', () => {
     renderWithTheme(<Logo color="black" />)
     expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({ color: '#030517' })
+  })
+
+  it('should render a bigger logo', () => {
+    renderWithTheme(<Logo size="large" />)
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({ width: '20rem' })
+  })
+
+  it('should render a bigger logo without text if hideOnMobile', () => {
+    renderWithTheme(<Logo hideOnMobile />)
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyleRule(
+      'width',
+      '5.8rem',
+      {
+        media: '(max-width: 768px)'
+      }
+    )
   })
 })
